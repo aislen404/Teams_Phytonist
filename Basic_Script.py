67040c375904c74ddd34ddab4bf085d133adf614
Jose Samuel Berigüete Valero <aislen404@gmail.com>
2023-06-07 18:59:51 +0200

    First version

---
+++ Basic_Script.py
@@ -0,0 +1,143 @@
+import requests
+import os
+
+# Variables globales
+user_id = None
+access_token = None
+group_id = None
+equipo_actual = None
+
+def login_usuario():
+    global user_id, access_token
+    print("== Proveer Access Token ==")
+    # token_file = input("Introduce el nombre del archivo que contiene el access_token: ")
+    token_file = 'access_token.txt'
+    try:
+        with open(token_file, 'r') as file:
+            access_token = file.read().strip()
+        user_id = obtener_user_id()
+        if user_id:
+            print("¡Inicio de sesión exitoso!")
+        else:
+            print("Error en el inicio de sesión. Verifica el access_token.")
+    except FileNotFoundError:
+        print("Archivo no encontrado. Verifica el nombre y la ubicación del archivo.")
+
+def obtener_user_id():
+    global access_token
+    headers = {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json'
+    }
+    response = requests.get('https://graph.microsoft.com/v1.0/me', headers=headers)
+    if response.status_code == 200:
+        return response.json().get('id')
+    return None
+
+def crear_equipo():
+    global equipo_actual
+    print("== Crear Teams ==")
+    display_name = input("Introduce el nombre del grupo: ")
+    description = input("Introduce la descripción del grupo: ")
+    group_data = {
+        'template@odata.bind': "https://graph.microsoft.com/v1.0/teamsTemplates('standard')",
+        'displayName': display_name,
+        'description': description,
+        'groupTypes': ['Unified']
+    }
+    headers = {
+        'Authorization': 'Bearer ' + access_token,
+        'Content-Type': 'application/json'
+    }
+    response = requests.post('https://graph.microsoft.com/v1.0/teams', headers=headers, json=group_data)
+    if response.status_code == 202:
+        location = response.headers['Location']
+        group_id_start = location.find("(") + 1
+        group_id_end = location.find(")")
+        group_id = location[group_id_start:group_id_end]
+        equipo_actual = group_id
+        print(f"Equipo '{display_name}' creado exitosamente.")
+        trabajar_equipo_actual()
+    else:
+        print("Error al crear el equipo:", response.text)
+
+def trabajar_equipo_actual():
+    global equipo_actual
+    print(f"\n=== TRABAJANDO SOBRE EL EQUIPO {equipo_actual} ===")
+    while True:
+        print("\n=== OPCIONES ===")
+        print("1. Crear Canal Público")
+        print("2. Crear Canal Privado")
+        print("3. Listar Canales Disponibles")
+        print("4. Volver al Menú Principal")
+
+        opcion = input("Selecciona una opción (1-4): ")
+
+        if opcion == '1':
+            crear_canal_publico()
+        elif opcion == '2':
+            crear_canal_privado()
+        elif opcion == '3':
+            listar_canales()
+        elif opcion == '4':
+            break
+        else:
+            print("Opción inválida. Intenta de nuevo.")
+
+def crear_canal_publico():
+    global equipo_actual
+    print("== Crear Canal Público ==")
+    canal_name = input("Introduce el nombre del canal público: ")
+    # Código para crear el canal público en el equipo actual
+    print(f"Canal público '{canal_name}' creado exitosamente.")
+
+def crear_canal_privado():
+    global equipo_actual
+    print("== Crear Canal Privado ==")
+    canal_name = input("Introduce el nombre del canal privado: ")
+    # Código para crear el canal privado en el equipo actual
+    print(f"Canal privado '{canal_name}' creado exitosamente.")
+
+def listar_canales():
+    global equipo_actual
+    print("== Listar Canales Disponibles ==")
+    # Código para obtener y mostrar la lista de canales del equipo actual
+    print("Lista de canales:")
+    # Mostrar los canales obtenidos
+
+def borrarPantalla(): #Definimos la función estableciendo el nombre que queramos
+    if os.name == "posix":
+       os.system ("clear")
+    elif os.name == "ce" or os.name == "nt" or os.name == "dos":
+       os.system ("cls")
+
+def volver_menu_principal():
+    input("\nPresiona Enter para volver al menú principal...")
+    mostrar_menu_principal()
+
+def mostrar_menu_principal():
+    while True:
+        print("\n=== MENÚ PRINCIPAL ===")
+        print("1. Proveer Access Token")
+        print("2. Crear Equipo")
+        print("3. Salir")
+
+        opcion = input("Selecciona una opción (1-3): ")
+
+        if opcion == '1':
+            login_usuario()
+        elif opcion == '2':
+            if user_id and access_token:
+                crear_equipo()
+            else:
+                print("Primero debes iniciar sesión.")
+        elif opcion == '3':
+            break
+        else:
+            print("Opción inválida. Intenta de nuevo.")
+
+        if opcion == '3':
+            break
+
+# Ejecución del script
+mostrar_menu_principal()
